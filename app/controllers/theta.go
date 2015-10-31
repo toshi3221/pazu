28ea5e5ac72b3caa3d25bf560a1e83b6df0e9e4f
Toshiharu Takematsu <info@newfish.jp>
2015-10-31 23:15:59 +0900

    Implement: Take Picture

---
+++ app/controllers/theta.go
@@ -0,0 +1,37 @@
+package controllers
+
+import (
+	"github.com/revel/revel"
+	"github.com/toshi3221/theta_v2"
+	"github.com/toshi3221/theta_v2/command"
+)
+
+type Theta struct {
+	*revel.Controller
+}
+
+func (c Theta) TakePicture() revel.Result {
+	client, _ := theta_v2.NewClient("http://192.168.1.1")
+
+	// camera.startSession
+	revel.INFO.Println("camera.startSession:")
+	startSessionCommand := new(command.StartSessionCommand)
+	client.CommandExecute(startSessionCommand)
+
+	revel.INFO.Println("  sessionId:", *startSessionCommand.Results.SessionId)
+	sessionId := startSessionCommand.Results.SessionId
+
+	// camera.takePicture
+	revel.INFO.Println("camera.takePicture")
+	takePictureCommand := new(command.TakePictureCommand)
+	takePictureCommand.Parameters.SessionId = sessionId
+	client.CommandExecute(takePictureCommand)
+
+	// camera.closeSession
+	revel.INFO.Println("camera.closeSession:")
+	closeSessionCommand := new(command.CloseSessionCommand)
+	closeSessionCommand.Parameters.SessionId = sessionId
+	client.CommandExecute(closeSessionCommand)
+
+	return c.Redirect(App.Index)
+}
